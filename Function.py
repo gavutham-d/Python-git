e657dfbd011cfc4029ee52ae020c461f2a1face2
gavutham-d <gavuthaman27@gmail.com>
2025-09-19 09:34:02 +0530

    FunctionCalc

---
+++ Function.py
@@ -0,0 +1,19 @@
+def op_marker():
+    n=int(input("Enter an Option: "))
+    if n>0 and n<5:
+       a=int(input())
+       b=int(input())
+       if n==1:
+        k=a+b
+       elif n==2:
+         k=a-b
+       elif n==3:
+         k=a*b
+       elif n==4:
+         k=a/b
+       elif n==5:
+         k=a%b
+    else:
+      k="Invalid Input"
+    print(k)
+op_marker()
